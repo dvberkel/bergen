c25316e76c845cfa737293eb9f5e4401b0e249f5
Daan van Berkel <Daan.vanBerkel@han.nl>
2018-03-15 07:02:14 +0100

    increment the instruction pointer

--- compiler/src/brnfck/machine.rs
+++ compiler/src/brnfck/machine.rs
@@ -3,6 +3,7 @@ use std::fmt::{self, Debug, Formatter};
 const SIZE: usize = 30_000;
 
 struct BuildMachine<'a> {
+	instruction_pointer: usize,
 	instructions: &'a [Command],
 	cell_pointer: usize,
 	cells: [u8; SIZE],	
@@ -10,20 +11,24 @@ struct BuildMachine<'a> {
 
 impl<'a> BuildMachine<'a> {
 	fn with(instructions: &'a[Command]) -> Self {
-		BuildMachine { instructions, cell_pointer: 0, cells: [0; SIZE] }
+		BuildMachine { instruction_pointer:0, instructions, cell_pointer: 0, cells: [0; SIZE] }
 	}
 
-	fn pointer_at(self, cell_pointer: usize) -> Self {
-		BuildMachine { instructions: self.instructions, cell_pointer, cells: self.cells }
+	fn instruction_pointer_at(self, instruction_pointer: usize) -> Self {
+		BuildMachine { instruction_pointer, instructions: self.instructions, cell_pointer: self.cell_pointer, cells: self.cells }
+	}
+
+	fn cell_pointer_at(self, cell_pointer: usize) -> Self {
+		BuildMachine { instruction_pointer: self.instruction_pointer, instructions: self.instructions, cell_pointer, cells: self.cells }
 	}
 
 	fn cell(mut self, index: usize, value: u8) -> Self {
 		self.cells[index] = value;
-		BuildMachine { instructions: self.instructions, cell_pointer: self.cell_pointer, cells: self.cells }
+		BuildMachine { instruction_pointer: self.instruction_pointer, instructions: self.instructions, cell_pointer: self.cell_pointer, cells: self.cells }
 	}
 
 	fn build(self) -> Machine<'a> {
-		Machine { instruction_pointer: 0, instructions: self.instructions, cell_pointer: self.cell_pointer, cells: self.cells }
+		Machine { instruction_pointer: self.instruction_pointer, instructions: self.instructions, cell_pointer: self.cell_pointer, cells: self.cells }
 	}
 }
 
@@ -43,17 +48,21 @@ impl<'a> Machine<'a> {
 		let command = self.instructions[self.instruction_pointer];
 		match command {
 			Command::IncrementPointer => {
+				self.instruction_pointer += 1;
 				self.cell_pointer = if self.cell_pointer  < SIZE - 1 { self.cell_pointer + 1 } else { 0 }; 
 			}
-			Command::DecrementPointer => { 
+			Command::DecrementPointer => {
+				self.instruction_pointer += 1;
 				self.cell_pointer = if self.cell_pointer != 0 { self.cell_pointer - 1 } else { SIZE - 1 }; 
 			}
-			Command::Increment => { 
+			Command::Increment => {
+				self.instruction_pointer += 1;
 				let current_value = self.cells[self.cell_pointer];
 				let value = if current_value != u8::max_value() { current_value + 1 } else { u8::min_value() };
 				self.cells[self.cell_pointer] = value;
 			}
-			Command::Decrement => { 
+			Command::Decrement => {
+				self.instruction_pointer += 1;
 				let current_value = self.cells[self.cell_pointer];
 				let value = if current_value != u8::min_value() { current_value - 1 } else { u8::max_value() };
 				self.cells[self.cell_pointer] = value;
@@ -120,10 +129,10 @@ mod tests {
 	fn execute_instruction() {
 		let instructions = [Command::IncrementPointer, Command::DecrementPointer, Command::Increment, Command::Decrement];
 		for (instruction, expected_machine) in vec![
-			(Command::IncrementPointer, BuildMachine::with(&instructions[0..1]).pointer_at(1).build()),
-			(Command::DecrementPointer, BuildMachine::with(&instructions[1..2]).pointer_at(SIZE - 1).build()),
-			(Command::Increment, BuildMachine::with(&instructions[2..3]).cell(0, 1).build()),
-			(Command::Decrement, BuildMachine::with(&instructions[3..4]).cell(0, u8::max_value()).build()),
+			(Command::IncrementPointer, BuildMachine::with(&instructions[0..1]).instruction_pointer_at(1).cell_pointer_at(1).build()),
+			(Command::DecrementPointer, BuildMachine::with(&instructions[1..2]).instruction_pointer_at(1).cell_pointer_at(SIZE - 1).build()),
+			(Command::Increment, BuildMachine::with(&instructions[2..3]).instruction_pointer_at(1).cell(0, 1).build()),
+			(Command::Decrement, BuildMachine::with(&instructions[3..4]).instruction_pointer_at(1).cell(0, u8::max_value()).build()),
 		] {
 			let instructions = [instruction];
 			let mut machine = Machine::new(&instructions);
@@ -139,10 +148,10 @@ mod tests {
 	#[test]
 	fn increment_pointer_wraps_around() {
 		let instructions = [Command::IncrementPointer];			
-		let mut machine = BuildMachine::with(&instructions).pointer_at(SIZE - 1).build();
+		let mut machine = BuildMachine::with(&instructions).cell_pointer_at(SIZE - 1).build();
 
 		if let Ok(result_machine) = machine.execute() {
-			assert_eq!(result_machine, BuildMachine::with(&instructions).pointer_at(0).build());
+			assert_eq!(result_machine, BuildMachine::with(&instructions).instruction_pointer_at(1).cell_pointer_at(0).build());
 		} else {
 			assert!(false);
 		}
@@ -154,7 +163,7 @@ mod tests {
 		let mut machine = BuildMachine::with(&instructions).cell(0, u8::max_value()).build();
 
 		if let Ok(result_machine) = machine.execute() {
-			assert_eq!(result_machine, BuildMachine::with(&instructions).cell(0,0).build());
+			assert_eq!(result_machine, BuildMachine::with(&instructions).instruction_pointer_at(1).cell(0,0).build());
 		} else {
 			assert!(false);
 		}
