13c8e366f39a38e81a7112245765f0cd1cc98a34
Daan van Berkel <Daan.vanBerkel@han.nl>
2018-03-16 13:48:48 +0100

    create a brnfck runner

--- compiler/src/brnfck/mod.rs
+++ compiler/src/brnfck/mod.rs
@@ -1,1 +1,29 @@
 mod machine;
+
+pub use self::machine::Command;
+
+pub fn run(instructions: &[machine::Command]) -> Result<(), machine::MachineError> {
+	let mut machine = machine::Machine::new(instructions);
+
+	while !machine.halted() {
+		match machine.execute() {
+			Ok(next_machine) => { machine = next_machine; },
+
+			Err(error) => { return Err(error); },
+		}
+	}
+
+	Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn run_simple_program() {
+		let instructions = [Command::Increment, Command::Increment, Command::JumpAhead, Command::Decrement, Command::JumpBack];
+		
+		assert_eq!(run(&instructions), Ok(()));
+	}
+}
