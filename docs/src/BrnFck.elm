b30d397127a41976519860248928dda01ad4a228
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2019-03-06 16:00:05 +0100

    Style registers

--- docs/src/BrnFck.elm
+++ docs/src/BrnFck.elm
@@ -4,13 +4,15 @@ import Array exposing (Array)
 import Css exposing (..)
 import Html as PlainHtml
 import Html.Styled as Html exposing (Html, toUnstyled)
-import Html.Styled.Attributes as Attribute
+import Html.Styled.Attributes as Attribute exposing (css)
 
 
 type Machine
     = Machine MachineState
 
-type alias Register = Int
+
+type alias Register =
+    Int
 
 
 type alias MachineState =
@@ -86,26 +88,67 @@ valueAt : Int -> Register -> Machine -> Machine
 valueAt pointer value (Machine ({ registers } as state)) =
     Machine { state | registers = registers |> Array.set pointer value }
 
+
 view : Machine -> Html msg
 view aMachine =
-    Html.div [ Attribute.class "machine"] [
-         viewRegisters aMachine
+    Html.div [ Attribute.class "machine" ]
+        [ viewRegisters aMachine
         ]
 
+
 viewRegisters : Machine -> Html msg
-viewRegisters (Machine {registers, pointer}) =
+viewRegisters (Machine { registers, pointer }) =
     let
         viewOfRegisters =
             registers
-            |> Array.indexedMap (viewRegister pointer)
-            |> Array.toList
+                |> Array.indexedMap (viewRegister pointer)
+                |> Array.toList
     in
-    Html.div [Attribute.class "registers"]
+    Html.div [ Attribute.class "registers" ]
         viewOfRegisters
+
 
 viewRegister : Int -> Int -> Register -> Html msg
 viewRegister pointer index register =
     let
-        label = String.fromInt register
+        label =
+            String.fromInt register
+
+        colorOfBackground =
+            if pointer == index then
+                black
+            else
+                white
+
+        colorOfText =
+            if pointer == index then
+                white
+            else
+                black
     in
-    Html.span [ Attribute.class "register"] [Html.text label]
+    Html.span
+        [ Attribute.class "register"
+        , css
+            [ display inlineBlock
+            , borderColor black
+            , borderStyle solid
+            , borderWidth (px 1)
+            , width (em 3)
+            , height (ex 3)
+            , lineHeight (ex 3)
+            , textAlign center
+            , backgroundColor colorOfBackground
+            , color colorOfText
+            , margin (px 2)
+            ]
+        ]
+        [ Html.text label ]
+
+
+black : Color
+black =
+    rgb 0 0 0
+
+white : Color
+white =
+    rgb 255 255 255
